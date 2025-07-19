b11a3bde8057a7b192cfc67468c28089b7635fe7
Bedanta004 <mahantybedanta@gmail.com>
2025-07-19 22:51:18 +0530

    Time: 289 ms (6.27%) | Memory: 161.9 MB (5.18%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -3,21 +3,25 @@ public:
     int reductionOperations(vector<int>& nums) {
         //sort(nums.begin(),nums.end());
         unordered_map<int,int> freq;
+        unordered_map<int, bool> visited;
 
         for(int num : nums){
             freq[num]++;
         }
         vector<int> a;
-        for(auto& i : freq){
-            a.push_back(i.first);
-        }
-        sort(a.begin(),a.end());
+        // for(auto& i : freq){
+        //     a.push_back(i.first);
+        // }
+        sort(nums.begin(),nums.end());
         int s=0, ans =0;
         int n = nums.size();
 
-        for(int num : a){
+        for(int num : nums){
+            if(!visited[num]){
             ans += n-freq[num]-s;
             s = s + freq[num];
+            visited[num] = true;
+            }
         }
         return ans;
     }

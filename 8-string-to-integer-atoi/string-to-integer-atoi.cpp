5d77e972eb070ddf9339a17bb273e92ae3945118
Bedanta004 <mahantybedanta@gmail.com>
2025-08-14 17:13:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (56.11%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,50 +1,27 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        string str = "";
-        bool started = false;
+        int i = 0, n = s.size();
+        long long num = 0;
+        int sign = 1;
 
-        for (int i = 0; i < s.size(); i++) {
-    if (str.size() == 0 && s[i] == ' ') continue;
+        // 1. Skip leading spaces
+        while (i < n && s[i] == ' ') i++;
 
-    else if (isalpha(s[i])) break;
+        // 2. Sign check
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') sign = -1;
+            i++;
+        }
 
-    else if (str.size() == 0 && (s[i] == '+' || s[i] == '-'))
-        str += s[i];
+        // 3. Parse digits and check overflow
+        while (i < n && isdigit(s[i])) {
+            num = num * 10 + (s[i] - '0');
+            if (num * sign >= INT_MAX) return INT_MAX;
+            if (num * sign <= INT_MIN) return INT_MIN;
+            i++;
+        }
 
-    else if (isdigit(s[i])) {
-        str += s[i];
-        continue;
-    }
-
-    else break;
-}
-
-// ✅ Fix 1: Check empty or only sign string
-if (str.empty() || (str.size() == 1 && (str[0] == '+' || str[0] == '-')))
-    return 0;
-
-// ✅ Fix 2: Manual conversion to prevent stoll overflow
-long long num = 0;
-int sign = 1;
-int idx = 0;
-if (str[0] == '+' || str[0] == '-') {
-    if (str[0] == '-') sign = -1;
-    idx = 1;
-}
-for (; idx < str.size(); idx++) {
-    int digit = str[idx] - '0';
-    if (num > (LLONG_MAX - digit) / 10)  // overflow check
-        return sign == 1 ? INT_MAX : INT_MIN;
-    num = num * 10 + digit;
-}
-num *= sign;
-
-// ✅ Fix 3: Clamp to int range
-if (num > INT_MAX) return INT_MAX;
-if (num < INT_MIN) return INT_MIN;
-
-return (int)num;
-
+        return (int)(num * sign);
     }
 };

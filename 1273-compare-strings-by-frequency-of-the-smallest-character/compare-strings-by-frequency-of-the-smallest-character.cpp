00ee2ee80cd16501150a2a0519bca6354703dd0f
Bedanta004 <mahantybedanta@gmail.com>
2025-08-21 00:28:06 +0530

    Time: 3 ms (63.90%) | Memory: 16 MB (56.36%) - LeetSync

---
+++ 1273-compare-strings-by-frequency-of-the-smallest-character/compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
+        vector<int> ans, a, b;
+
+        for(int i=0; i<queries.size(); i++){
+            string str = queries[i];
+            sort(str.begin(),str.end());
+            char ch = str[0];
+            int count = 0;
+            for(char chr : str){
+                if(chr == ch) count++;
+                else break;
+            }
+            a.push_back(count);
+        }
+
+        for(int i=0; i<words.size(); i++){
+            string str = words[i];
+            sort(str.begin(),str.end());
+            char ch = str[0];
+            int count = 0;
+            for(char chr : str){
+                if(chr == ch) count++;
+                else break;
+            }
+            b.push_back(count);
+        }
+        sort(b.begin(),b.end());
+        unordered_map<int,int> frq;
+
+        for (int x : a) {
+        auto it = upper_bound(b.begin(),b.end(), x);
+        ans.push_back(b.end() - it);
+    }
+    return ans;
+    }
+};

26e7fba02c7186453eb6f9cccf7b0d85a5f41c42
Bedanta004 <mahantybedanta@gmail.com>
2025-08-25 18:40:11 +0530

    Time: 86 ms (5.78%) | Memory: 8.4 MB (33.07%) - LeetSync

---
+++ 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxContainers(int n, int w, int maxWeight) {
+        int count = 0, m = n*n;
+
+        for(int i=0; i<=n*n; i++){
+            if(m > 0 && maxWeight-w >= 0){
+                count++;
+                maxWeight -= w;
+                m--;
+            }
+        }
+        return count;
+    }
+};

8906b049aa9007be681b9512f2e42498b0dcbbe4
Bedanta004 <mahantybedanta@gmail.com>
2025-08-29 16:32:33 +0530

    Time: 2 ms (28.38%) | Memory: 16.9 MB (24.75%) - LeetSync

--- 2476-check-distances-between-same-letters/check-distances-between-same-letters.cpp
+++ 2476-check-distances-between-same-letters/check-distances-between-same-letters.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     bool checkDistances(string s, vector<int>& distance) {
-        unordered_map<char,int> mp,mp1;
+        unordered_map<char,int> mp;
 
         for(int i=0; i<s.size(); i++){
+            if(mp.count(s[i]) && (i - mp[s[i]] - 1) != distance[s[i]-'a']) return false;
             mp[s[i]] = i;
-            if(!mp1.count(s[i])) mp1[s[i]] = i;
-        }
-        int j = 0;
-
-        for(char ch = 'a'; ch <= 'z'; ch++){
-            if(!mp.count(ch)) {
-             j++;
-             continue;
-            }
-            if(mp.count(ch) && mp[ch] - mp1[ch] -1 != distance[j]) return false;
-            j++;
         }
         return true;
     }

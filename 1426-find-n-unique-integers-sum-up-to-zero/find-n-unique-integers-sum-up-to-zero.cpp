585d06860cb761ca5930bb666fc3eb814a9a47f7
Bedanta004 <mahantybedanta@gmail.com>
2025-09-04 21:38:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (53.37%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> res;
         if(n == 0) return res;
         if(n%2 != 0) res.push_back(0);
-        for(int i=1;i<=floor(n/2);i++){
+        for(int i=1;i<=(n/2);i++){
             res.push_back(i);
             res.push_back(-i);
         }

0e76ae6e311dae6a0550cedbf7f032efd28ee26c
Bedanta004 <mahantybedanta@gmail.com>
2025-11-12 12:12:45 +0530

    Time: 102 ms (5.12%) | Memory: 35.2 MB (5.07%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+bool isSafe(int newX, int newY, vector<vector<char>>& grid, map<pair<int, int>, bool> &vis){
+    if(newX >= 0 && newY >= 0 && newX < grid.size() && newY < grid[0].size() && grid[newX][newY] == '1' && vis[{newX,newY}] == false){
+        return true;
+    }
+    return false;
+}
+void bfs(int srcX, int srcY, vector<vector<char>>& grid, map<pair<int, int>, bool> &vis){
+    queue<pair<int, int>> q;
+
+    //initial state
+    q.push({srcX, srcY});
+    vis[{srcX, srcY}] = true;
+
+    while(!q.empty()){
+        pair<int, int> frontNodePair = q.front();
+        q.pop();
+        int tempX = frontNodePair.first;
+        int tempY = frontNodePair.second;
+
+        int dx[] = {-1,0,1,0};//ROW =>up, right, down, left
+        int dy[] = {0, 1, 0, -1};//COL =>
+
+        for(int i=0; i<4; ++i){
+            int newX = tempX + dx[i];
+            int newY = tempY + dy[i];
+            if(isSafe(newX, newY, grid, vis)){
+                q.push({newX, newY});
+                vis[{newX, newY}] = 1;
+            }
+        }
+    }
+}
+    int numIslands(vector<vector<char>>& grid) {
+        // queue<pair<int, int> q;
+        map<pair<int, int>, bool> vis;
+        int cnt = 0;
+        int n = grid.size(), m = grid[0].size();
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!vis[{i,j}] && grid[i][j] == '1'){
+                    bfs(i, j, grid, vis);
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};

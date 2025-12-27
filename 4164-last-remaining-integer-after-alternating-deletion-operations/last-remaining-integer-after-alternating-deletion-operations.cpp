67f43b6a7d291c26b0379c3f186dd28c8aa98c7b
Bedanta004 <mahantybedanta@gmail.com>
2025-12-27 19:42:53 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (44.42%) - LeetSync

--- 4164-last-remaining-integer-after-alternating-deletion-operations/last-remaining-integer-after-alternating-deletion-operations.cpp
+++ 4164-last-remaining-integer-after-alternating-deletion-operations/last-remaining-integer-after-alternating-deletion-operations.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long lastInteger(long long n) {
-        long long mask = 0xAAAAAAAAAAAAAAAAULL;
+        long long mask = 0xAAAAAAAAAAAAAAUL;
         return (mask & (n-1)) + 1;
     }
 };
